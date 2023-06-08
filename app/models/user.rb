390ca50770ac702888151c9eff538f9f16f25930
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-06-08 12:16:54 +0000

    ゲストログイン機能練習

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
+  def self.guest
+    user = User.find_or_create_by('email: @example,com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
+end
